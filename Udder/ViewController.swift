61d97b1835fdb985a38fcae21da31cb761d4c1e3
IceCurrent <95679948+IceCurrent@users.noreply.github.com>
2022-06-05 23:30:08 +0530

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by shreyansh sharma on 05/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
